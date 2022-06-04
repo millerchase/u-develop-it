f1d6fc7166cc1e85547a295540fbad301a94faa4
CMiller <mlr.chase40@gmail.com>
2022-06-04 00:07:49 -0500

    add database setup; starts server setup

---
+++ db/seeds.sql
@@ -0,0 +1,14 @@
+INSERT INTO candidates (first_name, last_name, industry_connected)
+
+VALUES
+    ('Ronald', 'Firbank', 1),
+    ('Virginia', 'Woolf', 1),
+    ('Piers', 'Gaveston', 0),
+    ('Charles', 'LeRoi', 1),
+    ('Katherine', 'Mansfiled', 1),
+    ('Dora', 'Carrington', 0),
+    ('Eward', 'Bellamy', 0),
+    ('Montague', 'Summers', 1),
+    ('Ocatavia', 'Butler', 1 ),
+    ('Unica', 'Zurn', 1);
+
