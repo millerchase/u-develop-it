70e18e36477b1a6097aaceeaf2c45f5e17e65144
CMiller <mlr.chase40@gmail.com>
2022-06-06 03:46:25 -0500

    add parties table; add query to combined parties and candidates table where needed

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,14 +1,25 @@
-INSERT INTO candidates (first_name, last_name, industry_connected)
+INSERT INTO parties (name, description)
 
 VALUES
-    ('Ronald', 'Firbank', 1),
-    ('Virginia', 'Woolf', 1),
-    ('Piers', 'Gaveston', 0),
-    ('Charles', 'LeRoi', 1),
-    ('Katherine', 'Mansfiled', 1),
-    ('Dora', 'Carrington', 0),
-    ('Eward', 'Bellamy', 0),
-    ('Montague', 'Summers', 1),
-    ('Ocatavia', 'Butler', 1 ),
-    ('Unica', 'Zurn', 1);
+    ('JS Juggernauts', 'The JS Juggernauts eat, breathe, and sleep JavaScript. They can build everything you could ever want in JS, including a new kitchen sink.'),
+    ('Heroes of HTML', 'Want to see a mock-up turn into an actual webpage in a matter of minutes? Well, the Heroes of HTML can get it done in amtter of seconds.'),
+    ('Git Gurus', 'Need to resolve a merge conflice? The Git Gurus have your back. Nobody knows Git like thes folks do.')
+;
+
+INSERT INTO candidates (first_name, last_name, party_id, industry_connected)
+
+VALUES
+    ('Ronald', 'Firbank', 1, 1),
+    ('Virginia', 'Woolf', 1, 1),
+    ('Piers', 'Gaveston', 1, 0),
+    ('Charles', 'LeRoi', 2, 1),
+    ('Katherine', 'Mansfiled', 2, 1),
+    ('Dora', 'Carrington', 3, 0),
+    ('Eward', 'Bellamy', 3, 0),
+    ('Montague', 'Summers', 3, 1),
+    ('Ocatavia', 'Butler', 3, 1),
+    ('Unica', 'Zurn', NULL, 1)
+;
+
+
 
